497ed19fe0d9ef9a0dbdc40129f9d1054fb121f1
moman13 <35956421+moman13@users.noreply.github.com>
2023-05-01 12:32:12 +0300

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,11 +10,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
+  String? _newTaskContent;
   _HomePageState();
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+    print("Input Value : $_newTaskContent");
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: _deviceHeight * 0.15,
@@ -61,6 +63,25 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _addTaskButton() {
-    return FloatingActionButton(child: const Icon(Icons.add), onPressed: () {});
+    return FloatingActionButton(
+        child: const Icon(Icons.add), onPressed: _displayTaskPopup);
+  }
+
+  void _displayTaskPopup() {
+    showDialog(
+        context: context,
+        builder: (BuildContext builder) {
+          return AlertDialog(
+            title: const Text("Add New Task"),
+            content: TextField(
+              onSubmitted: (value) {},
+              onChanged: (value) {
+                setState(() {
+                  _newTaskContent = value;
+                });
+              },
+            ),
+          );
+        });
   }
 }
